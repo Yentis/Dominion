048bbaab6c19cef48b669536697067369fdbaf66
Renzie <omanarenzie@gmail.com>
2016-03-24 10:17:51 +0100

    Added files via upload

--- Source/Deck.java
+++ Source/Deck.java
@@ -15,12 +15,12 @@ import java.util.*;
 public class Deck {
     //Deck maken
     private ArrayList deck = new ArrayList();
+    private ArrayList hand = new ArrayList();
     //startDeck (7copper, 3 estate)
     
    
     public void StarterDeck()
     {
-        
         for (int i=0;i<7;i++)
         {
             deck.add("koper");
@@ -31,13 +31,26 @@ public class Deck {
             deck.add("landgoed");
                 
         }
+        deck.
         System.out.print(deck);
-        
         
     }
     
-    
+    public void schuifelen
     
     //Kaarten Toevoegen aan Deck
-   
+    public void setDeck()
+    {
+        
+    
+    
+    }
+    public void voegKaartToeAanHand(int aantalKaarten)
+    {
+        for(int i=0;i<aantalKaarten;i++){
+            hand.add(deck.get(i));
+            deck.remove(i);
+        }
+    
+    }
 }
