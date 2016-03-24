21f5bdb2b9f65f842a35da7ec4684ad14ee8deab
Yentis <yentl178@gmail.com>
2016-03-24 10:29:24 +0100

    Update Deck.java

--- Source/Deck.java
+++ Source/Deck.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dominion;
+package com.company;
 
 /**
  *
@@ -17,40 +12,43 @@ public class Deck {
     private ArrayList deck = new ArrayList();
     private ArrayList hand = new ArrayList();
     //startDeck (7copper, 3 estate)
-    
-   
+
+
     public void StarterDeck()
     {
         for (int i=0;i<7;i++)
         {
             deck.add("koper");
-        
+
         }
         for (int i=0;i<3;i++)
         {
             deck.add("landgoed");
-                
+
         }
-        deck.
-        System.out.print(deck);
-        
     }
-    
-    public void schuifelen
-    
+
+    public void schuifelen(){
+
+    }
+
     //Kaarten Toevoegen aan Deck
     public void setDeck()
     {
-        
-    
-    
+
     }
-    public void voegKaartToeAanHand(int aantalKaarten)
+    public void voegKaartToeAanDeck(int aantalKaarten, Kaart kaart)
     {
+        for(int i=0;i<aantalKaarten;i++){
+            deck.add(kaart);
+            spel.verwijderVanVeld(i);
+        }
+    }
+
+    public void voegKaartToeAanHand(int aantalKaarten){
         for(int i=0;i<aantalKaarten;i++){
             hand.add(deck.get(i));
             deck.remove(i);
         }
-    
     }
 }
