b9c15746f8185a8f7aa26a451850b4c03d202df2
Yentis <yentl178@gmail.com>
2016-04-14 10:27:42 +0200

    Update Deck.java

--- Source/Deck.java
+++ Source/Deck.java
@@ -19,44 +19,52 @@ public class Deck {
     private ArrayList aflegstapel = new ArrayList();
     private ArrayList vuilbak = new ArrayList();
     //startDeck (7copper, 3 estate)
-    
-   
-    public void StarterDeck()
+
+
+    public void starterDeck(Spel spel)
     {
-        for (int i=0;i<7;i++) //moet nog aangepast worden via databank
-        {
-            deck.add(i+1);       
+        int aantalkaarten = 0;
+
+        for(int i=0;i<spel.getGeldveld().size();i++){
+            if(Objects.equals(spel.getGeldveld().get(i).getNaam(), "Koper") && aantalkaarten < 7){
+                deck.add(spel.getGeldveld().get(i));
+                aantalkaarten++;
+                spel.getGeldveld().remove(i);
+            }
         }
-        for (int i=0;i<3;i++)
-        {
-            deck.add(i+8);               
+
+        for(int i=0;i<spel.getOverwinningsveld().size();i++){
+            if(Objects.equals(spel.getOverwinningsveld().get(i).getNaam(), "Landgoed") && aantalkaarten < 10){
+                deck.add(spel.getOverwinningsveld().get(i));
+                aantalkaarten++;
+                spel.getOverwinningsveld().remove(i);
+            }
         }
-        schudden(); 
+
+        schudden();
         //System.out.println("start(zonder shuffle)"+deck);        
         //System.out.println("start"+deck);
         //for(int i=0;i<deck.size();i++){
-        //System.out.println("start"+deck);//}
+        //System.out.println("start"+deck);
     }
-    
-    public void schudden()      
+
+    public void schudden()
     {
-       Collections.shuffle(deck);
+        Collections.shuffle(deck);
     }
-    
- 
+
+
     //Deck naar hand
     public void voegKaartToeAanHand(int aantalKaarten)
     {
         for(int i=0;i<aantalKaarten;i++){
             //System.out.println(i + "" + deck.get(i));
             hand.add(deck.get(i));
-            
-            
         }
         //System.out.println("Deck"+deck);
         verwijderKaartenUitDeck(aantalKaarten);
-        
     }
+
     public void verwijderKaartenUitDeck(int aantalKaarten)
     {
         for(int i=0;i<aantalKaarten;i++){
@@ -66,11 +74,8 @@ public class Deck {
             //System.out.println("Hand"+hand);
             //System.out.println("Deck"+deck);
         }
-        
-    //System.out.println("hand"+hand);
-    //System.out.println("deck"+deck);
-    
-    
+        //System.out.println("hand"+hand);
+        //System.out.println("deck"+deck);
     }
     //van hand naar Deck (bij het einde van de beurt
     public void legKaartenAf()
@@ -78,37 +83,37 @@ public class Deck {
         //System.out.println("legKaartenWeg");
         for (int i = 0; i<hand.size();i++)
         {
-            aflegstapel.add(hand.get(i)); 
+            aflegstapel.add(hand.get(i));
             //System.out.println("Hand"+hand);
             //System.out.println("aflegstapel"+aflegstapel);              
         }
-        
-        hand.clear();       
-        
+
+        hand.clear();
+
         //System.out.println("aflegstapel"+aflegstapel+"\nhand"+hand);
-    
+
     }
     //van aflegstapel terug naar deck
     public void kaartenTerugNaarDeck()
     {
         //System.out.println("deck"+deck);
-        if (deck.isEmpty()) 
+        if (deck.isEmpty())
         {
             for (int i = 0; i<aflegstapel.size();i++)
             {
                 deck.add(aflegstapel.get(i));
-                
+
             }
-            
+
             aflegstapel.clear();
             schudden();
-            
-        }   
+
+        }
         else{
             //System.out.println(deck.size());
             //System.out.println("deck nog gevuld");
         }
         //System.out.println("\ndeck"+deck);
-        
+
     }
 }
