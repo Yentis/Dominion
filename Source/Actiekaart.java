dfb77ce83256f27fc2748ccdd245cd5e142bb428
Yentis <yentl178@gmail.com>
2016-04-21 15:24:24 +0200

    Update Actiekaart.java

--- Source/Actiekaart.java
+++ Source/Actiekaart.java
@@ -12,25 +12,103 @@ public class Actiekaart {
     private List<Kaart> deck = new ArrayList();
     private List<Kaart> hand = new ArrayList();
     private List<Kaart> aflegstapel = new ArrayList();
+    
+    public void speelactiekaart(String naam, Speler speler, Spel spel){
+        switch(naam){
+            case "heks":
+                heks();
+                break;
+            case "kelder":
+                kelder(speler);
+                break;
+            case "kerk":
+                kerk(speler);
+                break;
+            case "gracht":
+                gracht(spel, speler);
+                break;
+            case "kanselier":
+                kanselier(spel, speler);
+                break;
+            case "dorp":
+                dorp(spel, speler);
+                break;
+            case "houthakker":
+                houthakker(speler);
+                break;
+            case "werkplaats":
+                werkplaats(spel);
+                break;
+            case "bureaucraat":
+                bureaucraat(spel, speler);
+                break;
+            case "feest":
+                feest(spel, speler);
+                break;
+            case "schutterij":
+                schutterij(spel, speler);
+                break;
+            case "geldverlener":
+                geldverlener(speler);
+                break;
+            case "ombouwen":
+                ombouwen(spel);
+                break;
+            case "smederij":
+                smederij(spel, speler);
+                break;
+            case "spion":
+                spion(spel, speler);
+                break;
+            case "dief":
+                dief(spel);
+                break;
+            case "troonzaal":
+                troonzaal(spel, speler);
+                break;
+            case "raadzaal":
+                raadzaal(spel, speler);
+                break;
+            case "festival":
+                festival(spel, speler);
+                break;
+            case "laboratorium":
+                laboratorium(spel, speler);
+                break;
+            case "bibliotheek":
+                bibliotheek(spel, speler);
+                break;
+            case "markt":
+                markt(spel, speler);
+                break;
+            case "mijn":
+                mijn(spel);
+                break;
+            case "avonturier":
+                avonturier(spel);
+                break;
+        }
+    }
 
     public void heks(){
         //geef de andere spelers 2 vloekkaarten
-
+        
     }
 
     public void kelder(Speler speler){
         //+1 actie, selecteer de kaarten die je wilt afleggen, voeg hetzelfde kaarten toe aan de hand vanuit deck
         speler.addActie(1);
+        //leg kaarten af
     }
 
-    public void kerk(Spel spel){
+    public void kerk(Speler speler){
         //plaats tot 4 kaarten in de vuilbak
-
+        
     }
 
-    public void gracht(Spel spel)
+    public void gracht(Spel spel, Speler speler)
     {
-        spel.voegKaartToe(2, deck, hand);
+        spel.voegKaartToe(2, speler.getDeck(), speler.getHand());
         //counter-card
     }
 
@@ -38,21 +116,21 @@ public class Actiekaart {
         //+2 geld
         speler.addGeld(2);
         //gooi deck in aflegstapel
-        spel.voegKaartToe(deck.size(),deck,aflegstapel);
+        spel.voegKaartToe(deck.size(), speler.getDeck(), speler.getAflegstapel());
 
     }
     public void dorp(Spel spel,Speler speler){
 
-        //+1kaart ok
-        spel.voegKaartToe(1, deck, hand);
+        //+1 kaart
+        spel.voegKaartToe(1, speler.getDeck(), speler.getHand());
         //+2 acties
         speler.addActie(2);
 
     }
-    public void houthakker(Spel spel, Speler speler){
-        //+1 koop ok
+    public void houthakker(Speler speler){
+        //+1 koop
         speler.addKoop(1);
-        //+2 geld ok
+        //+2 geld
         speler.addGeld(2);
 
     }
@@ -63,12 +141,11 @@ public class Actiekaart {
         //+1 actiekaart
         speler.addActie(1);
         //elk ander speler toont  een overwinningskaart en plaatst het op zijn deck (of toont een hand zonder overwinningskaarten)
-
-
+        
     }
     public void feest(Spel spel,Speler speler){
-        //naar thrash
-       // speler.verwijderKaart(this,); to be continued
+        //deze kaart naar thrash
+        // speler.verwijderKaart(this,); to be continued
         //neem kaart die max 5 geld kost
 
     }
@@ -95,7 +172,7 @@ public class Actiekaart {
             }
         }
     }
-    public void geldverlener(Spel spel){
+    public void geldverlener(Speler speler){
         //thrash koper
         //krijg +3 geldkaart
     }
@@ -103,50 +180,52 @@ public class Actiekaart {
         //select kaart -> thrash
         //krijg een kaart die tot 2 meer geld kost
     }
-    public void smederij(Spel spel){
-        spel.voegKaartToe(3,deck,hand);
-
+    public void smederij(Spel spel, Speler speler){
+        spel.voegKaartToe(3,speler.getDeck(),speler.getHand());
     }
 
-    public void spion(Spel spel){
-        spel.voegKaartToe(1,deck,hand);
+    public void spion(Spel spel, Speler speler){
+        spel.voegKaartToe(1,speler.getDeck(),speler.getHand());
         //elke speler bekijkt de bovenste kaart van de deck en de speler kan beslissen of het naar de aflegstapel gaat
     }
     public void dief(Spel spel){
-        //bekijk de 2 bovenste kaarten van elk persoon zijn deck en beslis of je de kaart wilt thrashen
+        //bekijk de 2 bovenste kaarten van elk persoon zijn deck en beslis of je de kaart wilt plaatsen op de aflegstapel
     }
-    public void troonzaal(Spel spel){
+    public void troonzaal(Spel spel, Speler speler){
         //kies een actiekaart
         //effect gekozen actiekaart*2
+        String actiekaart = "";
+        speelactiekaart(actiekaart, speler, spel);
+        speelactiekaart(actiekaart, speler, spel);
     }
     public void raadzaal(Spel spel,Speler speler){
-        spel.voegKaartToe(4,deck,hand);
+        spel.voegKaartToe(4,speler.getDeck(),speler.getHand());
         //+1 koop
         speler.addKoop(1);
     }
 
     public void festival(Spel spel,Speler speler){
         //+2 acties +1 kaart +2geld
-        spel.voegKaartToe(1,deck,hand);
-        speler.addGeld(2);
         speler.addActie(2);
+        spel.voegKaartToe(1,speler.getDeck(),speler.getHand());
+        speler.addGeld(2);
     }
     public void laboratorium(Spel spel,Speler speler){
-        spel.voegKaartToe(2, deck, hand);
+        spel.voegKaartToe(2, speler.getDeck(), speler.getHand());
         //+1actie
         speler.addActie(1);
     }
-    public void bibliotheek(Spel spel){
-        while (hand.size()>7){
-            spel.voegKaartToe(1,deck,hand);
+    public void bibliotheek(Spel spel, Speler speler){
+        while (speler.getHand().size()<7){
+            spel.voegKaartToe(1,speler.getDeck(),speler.getHand());
         }
     }
     public void markt(Spel spel,Speler speler){
-        spel.voegKaartToe(1,deck,hand);
+        spel.voegKaartToe(1,speler.getDeck(),speler.getHand());
         //+1geld +1 koop +1actie
-        speler.addActie(1);
-        speler.addKoop(1);
         speler.addGeld(1);
+        speler.addKoop(1);
+        speler.addActie(1);
     }
     public void mijn(Spel spel){
         //thrash een geldkaart en geef de geldkaart met 1 waarde meer
