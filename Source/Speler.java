469c96f92e81fae7ba408bac5a51bc53f51d5a5c
Yentis <yentl178@gmail.com>
2016-04-21 10:45:51 +0200

    Update Speler.java

--- Source/Speler.java
+++ Source/Speler.java
@@ -12,11 +12,34 @@ public class Speler {
     private List<Kaart> deck = new ArrayList();
     private List<Kaart> hand = new ArrayList();
     private List<Kaart> aflegstapel = new ArrayList();
+    private int geld;
+    private int koop;
+    private int actie;
+
+    public void addKoop(int x) { koop += x;}
+    
+    public void addActie(int x) { actie += x;}
+    
+    public List<Kaart> getAflegstapel() {
+        return aflegstapel;
+    }
+
+    public int getGeld() {
+        return geld;
+    }
+
+    public int getOverwinningspunten() {
+        return overwinningspunten;
+    }
 
     public Speler(String naam, int overwinningspunten)
     {
         this.naam = naam;
         this.overwinningspunten = overwinningspunten;
+    }
+
+    public void addGeld(int x){
+        geld += x;
     }
 
     public void addPunten(int x){
@@ -29,5 +52,14 @@ public class Speler {
 
     public List<Kaart> getDeck() {
         return deck;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void verwijderKaart(Kaart k, int i){
+        aflegstapel.add(k);
+        hand.remove(i);
     }
 }
