17b871626bc94e7d02c72c9edc38b5626ba51cb4
nielsvanwalleghem <niels.vanwalleghem@gmail.com>
2016-05-03 12:27:05 +0200

    Added files via upload

---
+++ PHP/htmlkopstartpagina.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Dominion</title>
+    <link rel="stylesheet" href="lib/css/reset.css"/>
+    <link rel="stylesheet" href="lib/css/stylestartpagina.css"/>
+
+</head>
+<body>
