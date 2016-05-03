8cfcba0493df2681058d22129cce94e35e329d22
nielsvanwalleghem <niels.vanwalleghem@gmail.com>
2016-05-03 12:27:42 +0200

    Added files via upload

---
+++ PHP/ToonFuncties.php
@@ -0,0 +1,55 @@
+<?php
+
+
+function startpagina()
+{
+    ?>
+
+    <audio controls autoplay loop id="muziek">
+        <source src="music/RuneScape%20Login%20Themes%202001-2014.mp3" type="audio/mpeg">
+    </audio>
+    <button id="toggleMuziek" class="kleineKnop"></button>
+    <button id="spelregelsKnop" class="kleineKnop"></button>
+    <div id="spelRegels">
+        <button id="volgendePagina" class="kleineKnop">>></button>
+        <ul class="carousel">
+
+        </ul>
+
+
+    </div>
+    <h1>Dominion</h1>
+
+    <form id="start">
+        <button type="submit" class="groteKnop" id="spelen"> Spelen</button>
+
+        <button type="button" class="groteKnop"> Afsluiten</button>
+    </form>
+
+    <form method="POST" class="hide" id="naamgeving">
+        <button id="vorige"><strong><<</strong>Vorige</button>
+        <h3>Kies uw naam</h3>
+        <div>
+            <label for="speler1">Speler 1:</label>
+            <input type="text" id="speler1" name="speler1"/>
+        </div>
+        <div>
+            <label for="speler2">Speler 2:</label>
+            <input type="text" id="speler2" name="speler2"/>
+        </div>
+
+        <button formmethod="get" formaction="Gamepagina.php" type="submit" class="startp2">Starten</button>
+
+    </form>
+
+    <?php
+}
+function gamepagina(){
+    ?>
+<p>greojgihrgoijgeioge</p>
+
+
+<?php
+}
+
+
