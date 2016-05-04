016f949fef38708b1be00bf86f2f50aa72fe328b
Renzie <omanarenzie@gmail.com>
2016-05-04 11:20:04 +0200

    Update Gamepagina.php

--- PHP/Gamepagina.php
+++ PHP/Gamepagina.php
@@ -1,17 +1,33 @@
 <?php
-require_once "htmlkopstartpagina.php";
-require_once "ToonFuncties.php";
+
+function gamepagina(){
+    require_once "htmlkopGame.php";
+    ?>
+
+    <h2>Speler 1: <?php echo  $_GET["speler1"]?></h2>
+    <h2>Speler 2: <?php echo  $_GET["speler2"]?></h2>
+
+    <h1>Het is <?php echo  $_GET["speler1"]?> zijn beurt</h1>  <--moet nog aangepast worden met java-->
+    <ul id="Overwinningskaart">
+        <li></li>
+        <li></li>
+        <li></li>
+        <--voeg hier de veldkaartenkaarten toe-->
+    </ul> <ul id="Actiekaart">
+        <--voeg hier de veldkaartenkaarten toe-->
+    </ul>
+    <ul id="Geldkaart">
+        <--voeg hier de veldkaartenkaarten toe-->
+    </ul>
 
 
-gamepagina();
+    <?php
+}
 
 
-
-
-
-
-
-
-
-require_once "htmlstaart.php";
-?>
+/**
+ * Created by PhpStorm.
+ * User: Renzie
+ * Date: 29/04/2016
+ * Time: 14:53
+ */
