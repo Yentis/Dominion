f4761be59f36c16f488ba823cc8e83786c0603cb
Renzie <omanarenzie@gmail.com>
2016-05-04 11:21:35 +0200

    Update ToonFuncties.php

--- PHP/ToonFuncties.php
+++ PHP/ToonFuncties.php
@@ -1,64 +1,53 @@
 <?php
-
-
 function startpagina()
 {
+    require_once "htmlkopStart.php";
     ?>
+    
 
     <audio controls autoplay loop id="muziek">
         <source src="music/RuneScape%20Login%20Themes%202001-2014.mp3" type="audio/mpeg">
     </audio>
-    <button id="toggleMuziek" class="kleineKnop"></button>
-    <button id="spelregelsKnop" class="kleineKnop"></button>
+    <input type="button" id="toggleMuziek" class="kleineKnop">
+    <input type="button" id="spelregelsKnop" class="kleineKnop" value="?">
     <div id="spelRegels">
-        <button id="volgendePagina" class="kleineKnop">>></button>
+        <input type="button" id="volgendePagina" class="kleineKnop" value=">>">
         <ul class="carousel">
 
         </ul>
 
 
     </div>
-    <h1>Dominion</h1>
+
 
     <form id="start">
-        <button type="submit" class="groteKnop" id="spelen"> Spelen</button>
+        <h1>Dominion</h1>
+        <input type="submit" class="groteKnop" id="spelen" value="Spelen">
 
-        <button type="button" class="groteKnop"> Afsluiten</button>
+        <input type="button" class="groteKnop" value="Aflsuiten">
     </form>
 
-    <form method="POST" class="hide" id="naamgeving">
-        <button id="vorige"><strong><<</strong>Vorige</button>
-        <h3>Kies uw naam</h3>
-        <div>
-            <label for="speler1">Speler 1:</label>
-            <input type="text" id="speler1" name="speler1"/>
-        </div>
-        <div>
-            <label for="speler2">Speler 2:</label>
-            <input type="text" id="speler2" name="speler2"/>
-        </div>
+    <form method="get" class="hide" id="naamgeving" action="dominionindex.php?actie=Gamepagina">
+        <input type="button" id="vorige" value="<< vorige">
+    <h3>Kies uw naam</h3>
 
-        <button formmethod="get" formaction="Gamepagina.php" type="submit" class="startp2">Starten</button>
+        <label for="speler1">Speler 1:</label>
+        <input type="text" id="speler1" name="speler1" maxlength="15"/>
 
+
+        <label for="speler2">Speler 2:</label>
+        <input type="text" id="speler2" name="speler2" maxlength="15"/>
+
+        <div>
+        <input type="submit" class="startp2" value="Spelen" name="jaknop">
+        </div>
     </form>
 
     <?php
+
 }
-function gamepagina(){
-    ?>
-<p>greojgihrgoijgeioge</p>
 
 
-<?php
-}
-function scorepagina(){
-    ?>
-    <h1>Game over</h1>
-    <h3>Spelers</h3> <h3>punten</h3>
-    <form>
-        <button formmethod="get" formaction="Dominionindex.php"  type="submit" class="startp2">Opnieuwspelen</button>
-        <button formmethod="get" formaction="Dominionindex.php" type="submit" class="startp2">Doorgaan</button>
-    </form>
-<?php
-}
+
+
 
