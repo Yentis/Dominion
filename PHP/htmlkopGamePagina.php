7e54bf46522aad4f1a6498aaf0342f4940b8fbf8
Renzie <omanarenzie@gmail.com>
2016-05-04 11:23:28 +0200

    Create htmlkopGamePagina.php

---
+++ PHP/htmlkopGamePagina.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Dominion</title>
+    <link rel="stylesheet" href="lib/css/reset.css"/>
+    <link rel="stylesheet" href="lib/css/styleGame.css"/>
+</head>
+<body>
