3c68eda5942420634a4b29652bee8b6aac726b5c
Renzie <omanarenzie@gmail.com>
2016-05-05 15:50:53 +0200

    Update Dominionindex.php

--- PHP/Dominionindex.php
+++ PHP/Dominionindex.php
@@ -1,27 +1,18 @@
-
 <?php
-
 require_once "ToonFuncties.php";
 require_once "Gamepagina.php";
 
-if(!isset($_GET["speler1"]) && !isset($_GET["speler2"])){
+if (isset($_GET["jaknop"])) {
+    if (empty($_GET["speler1"] && $_GET["speler2"])) {
+        //misschien de hoofdmenu van spelen en afsluiten wegdoen en direct starten met de naamformulier
+        startpagina();
+        echo("spelers niet geset");
+    } else {
+        gamepagina();
+    }
+} else {
     startpagina();
+
 }
-else {
-    gamepagina();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 require_once "htmlstaart.php";
 ?>
