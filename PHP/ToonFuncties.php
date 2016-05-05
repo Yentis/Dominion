50a2e5556b9ee03e87aee3642a23dd0893a22a06
Renzie <omanarenzie@gmail.com>
2016-05-05 15:49:58 +0200

    Update ToonFuncties.php

--- PHP/ToonFuncties.php
+++ PHP/ToonFuncties.php
@@ -3,7 +3,7 @@ function startpagina()
 {
     require_once "htmlkopstartpagina.php";
     ?>
-    
+
 
     <audio controls autoplay loop id="muziek">
         <source src="music/RuneScape%20Login%20Themes%202001-2014.mp3" type="audio/mpeg">
@@ -20,16 +20,18 @@ function startpagina()
     </div>
 
 
-    <form id="start">
+    <!--<form id="start">
         <h1>Dominion</h1>
         <input type="submit" class="groteKnop" id="spelen" value="Spelen">
 
         <input type="button" class="groteKnop" value="Aflsuiten">
-    </form>
+    </form>-->
 
-    <form method="get" class="hide" id="naamgeving" action="dominionindex.php?actie=Gamepagina">
+    <!--bovenstaande form in commentaar want anders ist fucked up-->
+    
+    <form method="get" id="naamgeving" action="dominionindex.php?actie=Gamepagina">
         <input type="button" id="vorige" value="<< vorige">
-    <h3>Kies uw naam</h3>
+        <h3>Kies uw naam</h3>
 
         <label for="speler1">Speler 1:</label>
         <input type="text" id="speler1" name="speler1" maxlength="15"/>
@@ -39,15 +41,9 @@ function startpagina()
         <input type="text" id="speler2" name="speler2" maxlength="15"/>
 
         <div>
-        <input type="submit" class="startp2" value="Spelen" name="jaknop">
+            <input type="submit" class="startp2" value="Spelen" name="jaknop">
         </div>
     </form>
 
     <?php
-
 }
-
-
-
-
-
