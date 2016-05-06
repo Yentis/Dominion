3a8b08a39ad7a0d178b126bb1687171c2a00b195
Renzie <omanarenzie@gmail.com>
2016-05-06 17:22:45 +0200

    Update ToonFuncties.php

--- PHP/ToonFuncties.php
+++ PHP/ToonFuncties.php
@@ -48,4 +48,34 @@ function startpagina()
     <?php
 }
 
-?>
+
+
+function gamepagina(){
+    require_once "htmlkopGame.php";
+    ?>
+
+    <h2>Speler 1: <?php echo  $_POST["speler1"]?></h2>
+    <h2>Speler 2: <?php echo  $_POST["speler2"]?></h2>
+
+    <h1>Het is <?php echo  $_POST["speler1"]?> zijn beurt</h1>  <!--moet nog aangepast worden met java-->
+    <ul id="Overwinningskaart" class="veld">
+        <li><img src="lib/Kaarten/province.jpg"> </li>
+        <li><img src="lib/Kaarten/duchy.jpg"></li>
+        <li><img src="lib/Kaarten/estate.jpg"></li>
+        <!--voeg hier de veldkaartenkaarten toe-->
+    </ul>
+    <ul id="Actiekaart" class="veld">
+
+        <!--voeg hier de veldkaartenkaarten toe-->
+    </ul>
+    <ul id="Geldkaart" class="veld">
+        <!--voeg hier de veldkaartenkaarten toe-->
+    </ul>
+
+    <ul id="hand">
+
+    </ul>
+    <?php
+}
+
+
