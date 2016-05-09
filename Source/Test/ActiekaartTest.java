73837e446e15d4e5a33248aef8ed75ebae771776
Renzie <omanarenzie@gmail.com>
2016-05-09 19:20:19 +0200

    nog nie af

---
+++ Source/Test/ActiekaartTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+import com.company.Kaart;
+import com.company.Spel;
+import com.company.Speler;
+import org.junit.Test;
+import java.util.*;
+/**
+ * Created by Renzie on 9/05/2016.
+ */
+public class ActiekaartTest {
+    @Test
+    public void speelactiekaart() throws Exception {
+        Spel spel = new Spel();
+        spel.maakKaarten();
+        Speler speler = new Speler("derp", 0);
+        spel.starterDeck(spel, speler);
+
+        for (Kaart k : spel.getAlleKaarten()) {
+            spel.voegKaartToe(3,k,,speler.getHand());
+            if (Objects.equals(k.getType(), "Actie") || Objects.equals(k.getType(), "Actie-Reactie") || Objects.equals(k.getType(), "Actie-Aanval")) {
+                System.out.println(k.getNaam() + " | " + i);
+
+            }
+        }
+    }
+}
