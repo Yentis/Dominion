7ca50b462cc3680535b71f9d3ff9f08b78758ece
Renzie <omanarenzie@gmail.com>
2016-05-09 22:27:57 +0200

    Update script.js

--- html/lib/js/script.js
+++ html/lib/js/script.js
@@ -1,68 +1,91 @@
-/**
- * Created by niels on 14/04/2016.
- */
-/**
- * Created by Renzie on 21/04/2016.
- */
-$(document).ready(function () {
+    /**
+     * Created by niels on 14/04/2016.
+     */
+    /**
+     * Created by Renzie on 21/04/2016.
+     */
+    $(document).ready(function () {
 
-    console.log('derp');
-    $("#spelregelsKnop").on("click",toonSpelRegels);
-    $("#spelen").on("click",toonNaamGeving);
-    $("#vorige").on("click",toonStartpagina);
-    $("#toggleMuziek").on("click",toggleMuziek);
-    $("#volgendePagina").on("click",next);
+        console.log('derp');
+        $("#spelregelsKnop").on("click",toonSpelRegels);
+        $("#spelen").on("click",toonNaamGeving);
+        $("#vorige").on("click",toonStartpagina);
+        $("#toggleMuziek").on("click",toggleMuziek);
+        $("#volgendePagina").on("click",next);
+        $("#vorigePagina").on("click",previous);
+        $("#speelSpel").on("click",naarGamePagina);
+        $("#sluitSpelregels").on("click",sluitSpelregels);
+    });
 
-});
+    var huidigeAfbeelding = 1;
+    var i = 2;
+    function addImages(){
+        for (var i = 1; i<9 ;i++){
+            html = '<li><img src=lib/images/Pagina';
+                html+=i;
+            html+='.PNG></li>';
 
-var huidigeAfbeelding = 1;
-var i = 2;
-function addImages(){
-    for (var i = 1; i<9 ;i++){
-        html = '<li><img src=lib/images/Pagina';
-        html+=i;
-        html+='.PNG></li>';
-
-        $(".carousel").append(html);
+            $(".carousel").append(html);
+        }
+        $(".carousel li:first").show();
     }
-    $(".carousel li:first").show();
-}
 
 
-var next = function (e) {
-    e.preventDefault();
-    if (huidigeAfbeelding < 9) {
-        huidigeAfbeelding++;
-        $('.carousel li:nth-child(' + (huidigeAfbeelding - 1) + ')').fadeOut(function(){
-            $('.carousel li:nth-child(' + huidigeAfbeelding + ')').fadeIn();
-        });
+    var next = function (e) {
+        e.preventDefault();
+        if (huidigeAfbeelding < 9) {
+            huidigeAfbeelding++;
+            $('.carousel li:nth-child(' + (huidigeAfbeelding - 1) + ')').fadeOut(function(){
+                $('.carousel li:nth-child(' + huidigeAfbeelding + ')').fadeIn();
+            });
+        }
+    };
+
+    var previous = function (e) {
+        e.preventDefault();
+        if (huidigeAfbeelding > 1) {
+            huidigeAfbeelding--;
+            $('.carousel li:nth-child(' + (huidigeAfbeelding + 1) + ')').fadeOut(function(){
+                $('.carousel li:nth-child(' + huidigeAfbeelding + ')').fadeIn();
+            });
+        }
+    };
+    var audio = document.getElementById('muziek');
+
+    function toggleMuziek(e){
+        e.preventDefault();
+        audio.muted = !audio.muted;
+        $(this).toggleClass('mute');
     }
-};
+    function toonSpelRegels(){
+        $("#spelRegels").slideToggle("slow");
+        addImages();
+        $("#spelregelsKnop").fadeOut();
+    }
 
-var audio = document.getElementById('muziek');
+    var sluitSpelregels = function () {
+        $("#spelRegels").slideToggle("slow");
+        $("#spelregelsKnop").fadeIn();
+    };
 
-function toggleMuziek(e){
-    e.preventDefault();
-    audio.muted = !audio.muted;
-    $(this).toggleClass('mute');
-}
-function toonSpelRegels(){
-    $("#spelRegels").slideToggle("slow");
-    addImages();
-    $("#spelregelsKnop:after").css("content","X");
+    function toonNaamGeving(e){
+        e.preventDefault();
+        $("#start").slideUp("slow");
+        $("#naamgeving").slideDown("slow");
+    }
+
+    function toonStartpagina(e){
+        e.preventDefault();
+        $("#start").slideDown("slow");
+        $("#naamgeving").slideUp("slow");
+    }
 
 
-}
+    function naarGamePagina() {
+        if($("#speler1").value == null && $("#speler2").value == null){
+            alert("namen niet gegeven");
+        }
+        return false;
 
-function toonNaamGeving(e){
-    e.preventDefault();
+    }
 
-    $("#start").slideUp("slow");
-    $("#naamgeving").slideDown("slow");
-}
-
-function toonStartpagina(e){
-    e.preventDefault();
-    $("#start").slideDown("slow");
-    $("#naamgeving").slideUp("slow");
-}
