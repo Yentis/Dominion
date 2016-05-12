baaef8ad4910f9cbbceb5024addfab9f4a9266d4
Yentis <yentl178@gmail.com>
2016-05-12 12:48:04 +0200

    kaart update

--- Source/src/com/company/Kaart.java
+++ Source/src/com/company/Kaart.java
@@ -4,13 +4,16 @@ package com.company;
  * Created by Yentl-PC on 22/03/2016.
  */
 public class Kaart {
+    //region Instance Variables
     private int nr;
     private String naam;
     private int kost;
     private String type;
     private String omschrijving;
     private int waarde;
+    //endregion
 
+    //region Constructors
     public Kaart(int nr, String naam, int kost, String type, String omschrijving, int waarde){
         this.nr = nr;
         this.naam = naam;
@@ -27,7 +30,9 @@ public class Kaart {
         omschrijving = "Placeholder";
         waarde = 0;
     }
+    //endregion
 
+    //region Properties
     public int getNr() {
         return nr;
     }
@@ -36,34 +41,23 @@ public class Kaart {
         return waarde;
     }
 
-    public void setNaam(String naam) {
-        this.naam = naam;
-    }
     public String getNaam() {
         return naam;
     }
 
-    public void setKost(int kost) {
-        this.kost = kost;
-    }
     public int getKost() {
         return kost;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
     public String getType() {
         return type;
     }
 
-    public void setOmschrijving(String omschrijving) {
-        this.omschrijving = omschrijving;
-    }
     public String getOmschrijving() {
         return omschrijving;
     }
-
+    //endregion
+    
     @Override
     public String toString() {
         return getNaam() + "\n" + getKost() + "\n" + getType() + "\n" + getOmschrijving();
