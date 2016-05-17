0cf10d36e55808ec06b875cf103c1857286631db
laurensvisser <laurens.visser@student.howest.be>
2016-05-17 10:29:27 +0200

    Delete htmlkopGamePagina.php

--- PHP/htmlkopGamePagina.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Dominion</title>
-    <link rel="stylesheet" href="lib/css/reset.css"/>
-    <link rel="stylesheet" href="lib/css/styleGame.css"/>
-</head>
-<body>
